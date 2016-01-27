4ab83e092ee1c9dddf222eb027356869b48c18d4
Mosab Ahmad <mahmad@zla.bs>
2016-01-28 01:42:48 +0200

    Silex setup is ready.

--- web/index.php
+++ web/index.php
@@ -5,19 +5,28 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 // Require symfony components.
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 // Initiate an object of Silex Application.
 $app = new Silex\Application();
+
+// Register templates directory.
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__ . '/../views',
+));
 
 // Set debug to true.
 $app['debug'] = true;
 
 // Set a default root for the home page.
-$app->get('/', function() {
+$app->get('/', function() use ($app) {
+	return $app['twig']->render('login.twig', array());
+});
 
-	$message = '<h2>Hello, world!</h2>';
-
-	return new Response($message, 200);
+// Set an endpoint for api
+$app->get('/api/1/', function(){
+	$result = array('msg' => 'Hello, world!');
+	return new JsonResponse($result);
 });
 
 // Run the application.
