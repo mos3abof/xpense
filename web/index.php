43b8179f80479bc64e0f11f0090b608aa6b643a5
Mosab Ahmad <mahmad@zla.bs>
2016-01-27 23:52:47 +0200

    Playing with index.php

--- web/index.php
+++ web/index.php
@@ -1,10 +1,24 @@
 <?php
 
-// web/index.php
-require_once __DIR__.'/../vendor/autoload.php';
+// Require composer autoloader.
+require_once __DIR__ . '/../vendor/autoload.php';
 
+// Require symfony components.
+use Symfony\Component\HttpFoundation\Response;
+
+// Initiate an object of Silex Application.
 $app = new Silex\Application();
 
-// ... definitions
+// Set debug to true.
+$app['debug'] = true;
 
+// Set a default root for the home page.
+$app->get('/', function() {
+
+	$message = '<h2>Hello, world!</h2>';
+
+	return new Response($message, 200);
+});
+
+// Run the application.
 $app->run();
